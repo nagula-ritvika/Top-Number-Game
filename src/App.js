7a14019f8335f77a824e24dd360503bdef2272bd
nagula-ritvika <nagula.ri@husky.neu.edu>
2019-01-17 16:15:28 -0500

    game done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,107 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { TopNumber } from "./TopNumber";
+import { Display } from "./Display";
+import { Target } from "./Target";
+import { random, clone } from "./helpers";
+import "./App.css";
+
+const fieldStyle = {
+  position: "absolute",
+  width: "40%",
+  // bottom: 20,
+  left: "30%",
+  height: "60%"
+};
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      game: false,
+      targets: {},
+      latestClick: 0
+    };
+
+    this.intervals = null;
+
+    this.hitTarget = this.hitTarget.bind(this);
+    this.startGame = this.startGame.bind(this);
+    this.endGame = this.endGame.bind(this);
+  }
+
+  createTarget = (key, ms) => {
+    ms = ms || random(500, 2000);
+    this.intervals.push(
+      setInterval(
+        function() {
+          let targets = clone(this.state.targets);
+          let num = random(1, 1000 * 1000);
+          targets[key] = targets[key] !== 0 ? 0 : num;
+          this.setState({ targets: targets });
+        }.bind(this),
+        ms
+      )
+    );
+  };
+
+  hitTarget = e => {
+    if (e.target.className !== "target") return;
+    let num = parseInt(e.target.innerText);
+    for (let target in this.state.targets) {
+      let key = Math.random().toFixed(4);
+      this.createTarget(key);
+    }
+    this.setState({ latestClick: num });
+  };
+
+  startGame = () => {
+    this.createTarget("first", 750);
+    this.setState({
+      game: true
+    });
+  };
+
+  endGame = () => {
+    this.intervals.forEach(int => {
+      clearInterval(int);
+    });
+    this.intervals = [];
+    this.setState({
+      game: false,
+      targets: {},
+      latestClick: 0
+    });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.latestClick < prevState.latestClick) {
+      this.endGame();
+    }
+  }
+
+  componentWillMount() {
+    this.intervals = [];
+  }
+
   render() {
+    let buttonStyle = {
+      display: this.state.game ? "none" : "inline-block"
+    };
+    let targets = [];
+    for (let key in this.state.targets) {
+      targets.push(<Target number={this.state.targets[key]} key={key} />);
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <TopNumber number={this.state.latestClick} game={this.state.game} />
+        <Display number={this.state.latestClick} />
+        <button onClick={this.startGame} style={buttonStyle}>
+          New Game
+        </button>
+        <div style={fieldStyle} onClick={this.hitTarget}>
+          {targets}
+        </div>
       </div>
     );
   }
