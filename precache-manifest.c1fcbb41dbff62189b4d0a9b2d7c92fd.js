20c0fee465c13ec7ed7d130f397567e072d7518a
nagula-ritvika <nagula.ri@husky.neu.edu>
2019-01-17 16:22:32 -0500

    Updates

---
+++ precache-manifest.c1fcbb41dbff62189b4d0a9b2d7c92fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26455152f9e3c17e1f53",
+    "url": "/Top-Number-Game/static/js/runtime~main.26455152.js"
+  },
+  {
+    "revision": "85913fde39ddd3e71e71",
+    "url": "/Top-Number-Game/static/js/main.85913fde.chunk.js"
+  },
+  {
+    "revision": "9d65a0d5e5a889113337",
+    "url": "/Top-Number-Game/static/js/1.9d65a0d5.chunk.js"
+  },
+  {
+    "revision": "85913fde39ddd3e71e71",
+    "url": "/Top-Number-Game/static/css/main.775bfd97.chunk.css"
+  },
+  {
+    "revision": "2ab7f35fe8a9727392c5cdf6e90f107c",
+    "url": "/Top-Number-Game/index.html"
+  }
+];
